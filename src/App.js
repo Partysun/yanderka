b72424c0683699f7fdcf8441e793748dc64eda5d
Yura Zatsepin <yura.zatsepin@gmail.com>
2017-06-20 16:23:52 +0500

    restructure app

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
-import React, { Component } from 'react';
-import { branch } from 'baobab-react/higher-order';   
+import React from 'react';
+import { 
+  branch
+} from 'baobab-react/higher-order';   
 import { 
   Route,
-  Redirect,
   NavLink as Link
 } from 'react-router-dom';
 import { 
@@ -11,15 +12,14 @@ import {
   lifecycle
 } from 'recompose';
 import signals from './signals.js';
-import Yamoney from './settings/yamoney.js';
 import Stats from './stats/stats.js';
+import Settings from './settings/settings.js';
 import './App.css';
 import './button.css';
 import './streamlab.actions.js';
 import './yamoney.actions.js';
 import './donations.actions.js';
 import './alertbox.actions.js';
-import AlertboxSettings from './settings/alertbox-settings.js';
 
 const AppNavigation = ({user}) => (
   <div className='app-navigation'>
@@ -59,110 +59,29 @@ const AppHeader = ({user}) => (
   </section>
 )
 
-class StreamlabsOauth extends Component {
-
-  componentDidMount() {
-    const { location } = this.props;
-    signals.emit('user:connect:streamlabs:saveToken', location.search);
-  }
-
-  render() {
-    if (this.props.tokenSaved) {
-      return (                                                                              
-        <Redirect to='/' />                                                              
-      )
-    }
-    return (
-      <h2>
-        Настраиваем Streamlabs...
-      </h2>
-    )
-  }
-}
-
-class App extends Component {
-
-  render() {
-    const { user, donations, ui } = this.props;
-    return (
-      <div className='app'>
-        <div className='app-inner'>
-          <AppNavigation />
-          <AppHeader user={user} />
-          <br />
-          <Route                                                                              
-            path='/'                                                                     
-            exact
-            render={() => (
-              <Stats donations={donations} />
-            )}
-          />
-            <Route                                                                              
-              path='/settings'                                                                     
-              exact
-              render={() => (
-                <div>
-                  <AlertboxSettings user={user} />
-                  <br />
-                  <section className='streamlabs-settings'>                                                            
-                    <header>                                                                            
-                      <div className='copy'>                                                            
-                        <h2>Настройки Streamlabs.com</h2>                                                            
-                      </div>                                                                            
-                      {ui.streamlabs.tokenSaved &&
-                        <div className='actions'>                                                            
-                          <span className='label label-success'>Подключен</span>
-                        </div>                                                                            
-                      }
-                    </header>                                                                           
-                    <main>                                                                              
-                      {ui.streamlabs.tokenSaved ? 
-                        <div>
-                          <button                                                                         
-                            className='btn-secondary'                                                       
-                            onClick={() => signals.emit('streamlabs:alert')}> 
-                            Отправить тестовое сообщение
-                          </button>
-                          &nbsp;&nbsp;
-                          <button                                                                         
-                            className='btn-secondary'                                                       
-                            onClick={() => signals.emit('streamlabs:makeDonation')}> 
-                            Отправить тестовый донат
-                          </button>
-                        </div>
-                      :
-                      <button                                                                         
-                        className='btn-primary'                                                       
-                        style={{'display': 'flex', 'alignItems': 'center'}}
-                        onClick={() => signals.emit('user:connect:streamlabs')}> 
-                        Подключить Streamlabs.com
-                        <img 
-                          style={{'marginLeft': '1em'}}
-                          src='https://cdn-images-1.medium.com/fit/c/25/25/1*XhBKvE_pXEEM2pVt2b5jSg.png' alt='логотип streamlabs '
-                        />
-                      </button>
-                      }
-                      <Route 
-                        path='/oauth/streamlabs'
-                        render={({location}) => (
-                          <StreamlabsOauth 
-                            tokenSaved={ui.streamlabs.tokenSaved} 
-                            location={location}
-                          />
-                        )}
-                      />
-                    </main>                                                                             
-                  </section>
-                  <br />
-                  <Yamoney ui={ui} user={user} />
-                </div>
-              )}
-            />
-        </div>
-      </div>
-    );
-  }
-}
+const App = ({ user, donations, ui }) => (
+  <div className='app'>
+    <div className='app-inner'>
+      <AppNavigation />
+      <AppHeader user={user} />
+      <br />
+      <Route                                                                              
+        path='/'                                                                     
+        exact
+        render={() => (
+          <Stats donations={donations} />
+        )}
+      />
+      <Route                                                                              
+        path='/settings'                                                                     
+        exact
+        render={() => (
+          <Settings user={user} ui={ui} />
+        )}
+      />
+    </div>
+  </div>
+);
 
 const enhance = compose(
   pure,
---
+++ src/settings/settings.js
@@ -0,0 +1,89 @@
+import React, {Component} from 'react';
+import { 
+  Route,
+  Redirect,
+} from 'react-router-dom';
+import AlertboxSettings from './alertbox-settings.js';
+import Yamoney from './yamoney.js';
+import signals from './../signals.js';
+
+class StreamlabsOauth extends Component {
+
+  componentDidMount() {
+    const { location } = this.props;
+    signals.emit('user:connect:streamlabs:saveToken', location.search);
+  }
+
+  render() {
+    if (this.props.tokenSaved) {
+      return (                                                                              
+        <Redirect to='/' />                                                              
+      )
+    }
+    return (
+      <h2>
+        Настраиваем Streamlabs...
+      </h2>
+    )
+  }
+}
+
+const Settings = ({user, ui}) => (
+  <div>
+    <AlertboxSettings user={user} />
+    <br />
+    <section className='streamlabs-settings'>                                                            
+      <header>                                                                            
+        <div className='copy'>                                                            
+          <h2>Настройки Streamlabs.com</h2>                                                            
+        </div>                                                                            
+        {ui.streamlabs.tokenSaved &&
+          <div className='actions'>                                                            
+            <span className='label label-success'>Подключен</span>
+          </div>                                                                            
+        }
+      </header>                                                                           
+      <main>                                                                              
+        {ui.streamlabs.tokenSaved ? 
+          <div>
+            <button                                                                         
+              className='btn-secondary'                                                       
+              onClick={() => signals.emit('streamlabs:alert')}> 
+              Отправить тестовое сообщение
+            </button>
+            &nbsp;&nbsp;
+            <button                                                                         
+              className='btn-secondary'                                                       
+              onClick={() => signals.emit('streamlabs:makeDonation')}> 
+              Отправить тестовый донат
+            </button>
+          </div>
+        :
+        <button                                                                         
+          className='btn-primary'                                                       
+          style={{'display': 'flex', 'alignItems': 'center'}}
+          onClick={() => signals.emit('user:connect:streamlabs')}> 
+          Подключить Streamlabs.com
+          <img 
+            style={{'marginLeft': '1em'}}
+            src='https://cdn-images-1.medium.com/fit/c/25/25/1*XhBKvE_pXEEM2pVt2b5jSg.png' alt='логотип streamlabs '
+          />
+        </button>
+        }
+        <Route 
+          path='/oauth/streamlabs'
+          render={({location}) => (
+            <StreamlabsOauth 
+              tokenSaved={ui.streamlabs.tokenSaved} 
+              location={location}
+            />
+          )}
+        />
+      </main>                                                                             
+    </section>
+    <br />
+    <Yamoney ui={ui} user={user} />
+  </div>
+)
+
+export default Settings;
