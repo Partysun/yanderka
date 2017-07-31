76d052533d336c1e16ff46f88058703b225822e0
Yura Zatsepin <yura.zatsepin@gmail.com>
2017-08-01 01:45:48 +0500

    hard fix firebase cloud function hooks. Problem with firebase API update

--- functions/cloud.js
+++ functions/cloud.js
@@ -1,6 +1,12 @@
 const functions = require('firebase-functions');
 const admin = require('firebase-admin');
 
-admin.initializeApp(functions.config().firebase);
+// Why I should use custom credentials? Read it on stackoverflow.
+// https://stackoverflow.com/questions/42717540/firebase-cloud-functions-createcustomtoken
+const serviceAccount = require("./service-account.json");
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL: "https://yanderka-f39f7.firebaseio.com"
+});
 
 module.exports = admin;
